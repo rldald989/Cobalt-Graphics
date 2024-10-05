287942e1d9cf0be865c603aad9abc547d57b20d5
rldald989 <70861085+rldald989@users.noreply.github.com>
2024-10-05 18:32:48 -0500

    Update Utils.h

--- Utils.h
+++ Utils.h
@@ -3,11 +3,11 @@
 #include <Camera.h>
 #include <vector>
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+static void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
     glViewport(0, 0, width, height);
 }
 
-bool CheckCollision(Cobalt::GameObject& a, Cobalt::GameObject& b) {
+static bool CheckCollision(Cobalt::GameObject& a, Cobalt::GameObject& b) {
     bool colX = a.GetPosition().x + a.GetBounds().x >= b.GetPosition().x &&
         b.GetPosition().x + b.GetBounds().x >= a.GetPosition().x;
 
@@ -17,7 +17,7 @@ bool CheckCollision(Cobalt::GameObject& a, Cobalt::GameObject& b) {
     return colX && colY;
 }
 
-bool CheckRadialCollision(Cobalt::GameObject& a, Cobalt::GameObject& b, float dist)
+static bool CheckRadialCollision(Cobalt::GameObject& a, Cobalt::GameObject& b, float dist)
 {
     bool col = glm::abs(glm::distance(a.GetPosition(), b.GetPosition())) <= dist;
     return col;
@@ -39,6 +39,9 @@ struct Player : public Entity
     float runes = 0;
     float speed;
     bool bounce = false;
+    bool horizontal = false;
+    bool vertical = false;
+    glm::vec2 localPos = glm::vec2(0.0f);
 
     Player(Cobalt::GameObject* _obj, float _health, glm::vec2 _size, glm::vec2 _position, glm::vec2 _m_mov) : Entity({ _obj, _health, _size, _position, _m_mov })
     {
@@ -54,29 +57,35 @@ struct Player : public Entity
     void Controls(GLFWwindow* win)
     {
         if (health > 0) {
-            if (glfwGetKey(win, GLFW_KEY_W))
+            if (glfwGetKey(win, GLFW_KEY_W) && m_mov.x == 0)
             {
                 m_mov.y = speed;
+                vertical = true;
             }
-            else if (glfwGetKey(win, GLFW_KEY_S))
+            else if (glfwGetKey(win, GLFW_KEY_S) && m_mov.x == 0)
             {
                 m_mov.y = -speed;
+                vertical = true;
             }
             else {
                 if (!bounce) {
                     m_mov.y = 0.0f;
                 }
+                vertical = false;
             }
 
-            if (glfwGetKey(win, GLFW_KEY_A))
+            if (glfwGetKey(win, GLFW_KEY_A) && m_mov.y == 0)
             {
                 m_mov.x = -speed;
+                horizontal = true;
             }
-            else if (glfwGetKey(win, GLFW_KEY_D))
+            else if (glfwGetKey(win, GLFW_KEY_D) && m_mov.y == 0)
             {
                 m_mov.x = speed;
+                horizontal = true;
             }
             else {
+                horizontal = false;
                 if (!bounce) {
                     m_mov.x = 0.0f;
                 }
@@ -103,8 +112,87 @@ struct Player : public Entity
         //Multiplies x and y by the speed which is then multiplied by the speedMod to let us bounce off of objects or enemies
         m_mov = glm::vec2( (xDist * speed) * speedMod, (yDist * speed) * speedMod);
 
+        //   |
+        //-- . --
+        //   |
+        //
+
         //IDK How tf I got this to work
     }
 
 
+};
+
+enum AnimationState {
+    FORWARD = 1,
+    BACKWARDS = -1
+};
+
+class SpriteAnimator
+{
+public:
+    SpriteAnimator(int tileX, int tileY, Cobalt::GameObject* spriteContainer) : m_frameInd(0), m_frameUp(0),
+        m_frameLimit(5), m_sheetSize(tileX, tileY), m_spriteContainer(spriteContainer)
+    {
+        m_spriteIncr = glm::vec2(1.0f / tileX, 1.0f / tileY);
+        spriteContainer->SetTextureScale(m_spriteIncr.x, m_spriteIncr.y);
+    }
+    ~SpriteAnimator() {
+
+    }
+
+    void Update() {
+        m_frameUp++;
+    }
+
+    void Logic(AnimationState animState, int row) {
+        if (animState == FORWARD && m_frameUp == m_frameLimit) {
+            m_frameInd = m_frameInd + m_spriteIncr.x;
+            m_spriteContainer->GetShader()->SetVec2("offset", glm::vec2(m_frameInd, m_spriteIncr.y * row));
+
+        }
+        else if (animState == BACKWARDS && m_frameUp == m_frameLimit) {
+            m_frameInd = m_frameInd - m_spriteIncr.x;
+            m_spriteContainer->GetShader()->SetVec2("offset", glm::vec2(m_frameInd, (m_spriteIncr.y * row) - m_spriteIncr.y * 2));
+        }
+        else if (m_frameUp >= m_frameLimit + 1)
+        {
+            m_frameUp = 0;
+        }
+
+        // ref
+        // if (frameUp == frameLimit) {
+        //     m_frameInd = frameInd - 0.125f;
+        //     player.obj->GetShader()->SetVec2("offset", glm::vec2(frameInd, 0.0f));
+        // }
+        // 
+        // if (glfwGetKey(win.GetGLFWwindow(), GLFW_KEY_S) == GLFW_PRESS && frameUp == frameLimit) {
+        //     frameInd = frameInd + 0.125f;
+        //     player.obj->GetShader()->SetVec2("offset", glm::vec2(frameInd, 0.0f));
+        // }
+
+
+        if (m_frameInd > 8) {
+            m_frameInd = 0;
+        }
+    }
+
+    void Logic(int row) 
+    {
+        m_spriteContainer->GetShader()->SetVec2("offset", glm::vec2(0, (m_spriteIncr.y * row) - m_spriteIncr.y));
+    }
+
+    void SetFrameLimit(int frameLimit)
+    {
+        m_frameLimit = frameLimit;
+    }
+
+private:
+    float m_frameInd;
+    int m_frameUp;
+    int m_frameLimit;
+
+    glm::vec2 m_sheetSize;
+    glm::vec2 m_spriteIncr;
+    Cobalt::GameObject* m_spriteContainer;
 };
